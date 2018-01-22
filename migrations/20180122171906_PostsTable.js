d9d7920146cd7d79eb78e5bfe3e0dceddebbcb62
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-22 18:03:13 -0500

    Have all tables setup in migration

--- migrations/20180122171906_PostsTable.js
+++ migrations/20180122171906_PostsTable.js
@@ -10,10 +10,20 @@ exports.up = function (knex, Promise) {
       tbl.integer('userId').references('users.id');
       tbl.text('text').required();
       tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTable('tags', tbl => {
+      tbl.increments('id');
+      tbl.string('tag', 16).unique();
+      tbl.timestamp('createdAt').defaultTo(knex.fn.now());
     })
   ]);
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+  return Promise.all([
+  knex.schema.dropTableIfExists('users'),
+  knex.schema.dropTableIfExists('posts')
+  knex.schema.dropTableIfExists('tags')
+  ]);
+
 };
