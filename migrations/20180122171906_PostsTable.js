198d6d63b3041eda5423e489e32b89c131ab517e
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-22 19:35:30 -0500

    Schema setup and migrated to SQL

--- migrations/20180122171906_PostsTable.js
+++ migrations/20180122171906_PostsTable.js
@@ -1,29 +1,43 @@
 exports.up = function (knex, Promise) {
   return Promise.all([
     knex.schema.createTable('users', tbl => {
-      tbl.increments('id').primary();
-      tbl.string('name', 128).unique('user_name', 'uq_users_name');
+      tbl.increments('id');
+      tbl.string('name', 128).notNullable();
       tbl.timestamp('createdAt').defaultTo(knex.fn.now());
     }),
     knex.schema.createTable('posts', tbl => {
-      tbl.increments('id').primary();
-      tbl.integer('userId').references('users.id');
-      tbl.text('text').required();
+      tbl.increments('id');
+      tbl.integer('userId').references('id').inTable('users');
+      tbl.text('text').notNullable();
       tbl.timestamp('createdAt').defaultTo(knex.fn.now());
     }),
     knex.schema.createTable('tags', tbl => {
       tbl.increments('id');
-      tbl.string('tag', 16).unique();
+      tbl.string('tag', 16).unique('tag', 'uq_tag_name');
       tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+    }),
+    knex.schema.createTable('posttags', tbl => {
+      tbl.increments();
+      tbl
+        .integer('postId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('posts');
+      tbl
+        .integer('tagId')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('tags');
     })
   ]);
 };
 
 exports.down = function (knex, Promise) {
   return Promise.all([
-  knex.schema.dropTableIfExists('users'),
-  knex.schema.dropTableIfExists('posts')
-  knex.schema.dropTableIfExists('tags')
+    knex.schema.dropTableIfExists('users'),
+    knex.schema.dropTableIfExists('posts'),
+    knex.schema.dropTableIfExists('tags')
   ]);
-
 };
